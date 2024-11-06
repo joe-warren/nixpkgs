d5fcc0e518a62a0a55bef378a50c524b7b0d3678
zainab-ali <zainab@kebab-ca.se>
2024-11-06 16:08:38 +0000

    Fix opencascade-hs.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -3062,4 +3062,9 @@ self: super: {
      '';
   }) super.quickcheck-state-machine;
 
+  # opencascade-hs requires opencascade-occt
+  opencascade-hs = let occt = pkgs.opencascade-occt;
+  in appendConfigureFlag "--extra-include-dirs=${occt}/include/opencascade"
+    (addExtraLibrary occt super.opencascade-hs);
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
