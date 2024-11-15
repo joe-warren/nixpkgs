cd9fb052315830cc033a41a8d1e2efeff494ea2b
Joseph Warren <hungryjoe.warren@gmail.com>
2024-11-15 01:59:46 +0000

    Fix opencascade-hs
    As of [this PR](https://github.com/NixOS/cabal2nix/pull/630)
    opencascade-hs does not require the dependency on opencascade-occt to be manually specified.
    
    It does however still require the include path to be defined.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -3052,4 +3052,8 @@ self: super: {
     })
   ] super.hailgun;
 
+  # opencascade-hs requires the include path configuring relative to opencascade-occt
+  opencascade-hs = let occt = pkgs.opencascade-occt;
+    in appendConfigureFlag "--extra-include-dirs=${occt}/include/opencascade" super.opencascade-hs;
+
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
